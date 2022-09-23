3fffc48c20992c737a7a45286105e6257a9e33df
popnerdstudios <gajulas@oregonstate.edu>
2022-09-23 10:40:38 -0700

    styling for MacOS
    Got rid of x_button; Rounded border for input, buttons, selections, etc.

--- signgen/src/main.js
+++ signgen/src/main.js
@@ -424,13 +424,48 @@ document.getElementById("lm-home").addEventListener("click", function(){
 document.getElementById("lm-refresh").addEventListener("click", function(){
     document.getElementById("splashScreen").style.display = 'none';
     document.getElementById("canvas_a").style.opacity = "100";
-});
-
-    
-const {remote} = require('electron').remote;
-document.getElementById("x_button").addEventListener("click", function (){
-    console.log("cxxxx");
-    var window = remote.getCurrentWindow();
-    window.destroy();
 }); 
-    
+
+/*
+// Make the DIV element draggable:
+dragElement(document.getElementById("mainCanvas"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  elmnt.onmousedown = dragMouseDown;
+  
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+*/
+
+
+//FONT: { Sora } WORDS: { PAZ, , } COLORS: { #386641, #bc4749, #f2e8cf } { W: 633 H: 291 }
+//^^MISALLIGBED FONT
