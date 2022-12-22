c898ac03d81a791388cf4cf05d210dd6f3783ccc
sreegajula <realpopnerd@gmail.com>
2022-12-21 20:45:53 -0800

    dynamic sizinf w fabric

--- signgen/src/js/canvas.js
+++ signgen/src/js/canvas.js
@@ -1,26 +1,41 @@
 const r = new random();
+var canvas_back = new fabric.Canvas("canvas_back");
 
 class canvas {
 
     async generate() {
-        document.getElementById("splashScreen").style.display = 'none';
-        var canvas_back = new fabric.Canvas("canvas_back");
+        this.clear();
 
-        canvas_back.setWidth(633);
-        canvas_back.setHeight(291);
-        
+        var w = document.getElementById("CanvasWidth").value;
+        var h = document.getElementById("CanvasHeight").value;
+        if (w/h >= 0.25 && w/h <= 4) {
+            canvas_back.setWidth(w);
+            canvas_back.setHeight(h);
+        } else{
+            w = 633, h = 291 
+            canvas_back.setWidth(633);
+            canvas_back.setHeight(291);
+        }
+
+
         var colors = await r.ApplyColors();
         canvas_back.backgroundColor = colors.m1;
 
         var words = await r.GetRandomWord();
         var font = await r.ApplyFont();
-        canvas_back.add(new fabric.Text(words[0], { 
-            fontFamily: font, 
-            left: 100, 
-            top: 100 
-          }));
 
-        //canvas_back.renderAll();
+        console.log(w);
+        var text = new fabric.Textbox(words[0], {
+            width: Number(w),
+            textAlign: "center"
+        });
+  
+        canvas_back.add(text);
+    }
+
+    async clear(){
+        document.getElementById("splashScreen").style.display = 'none';
+        canvas_back.clear();
     }
 }
 
