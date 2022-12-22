774cd07f2036fa05a98118753423594c2c9bf409
Vishal Siddireddy <vsiddireddy@gmail.com>
2022-12-21 19:56:27 -0800

    fixed undefined issue

--- signgen/src/js/canvas.js
+++ signgen/src/js/canvas.js
@@ -5,17 +5,22 @@ class canvas {
     async generate() {
         document.getElementById("splashScreen").style.display = 'none';
         var canvas_back = new fabric.Canvas("canvas_back");
+
+        canvas_back.setWidth(633);
+        canvas_back.setHeight(291);
         
         var colors = await r.ApplyColors();
         canvas_back.backgroundColor = colors.m1;
 
         var words = await r.GetRandomWord();
         var font = await r.ApplyFont();
-        canvas_back.add(new fabric.Text(words[1], { 
+        canvas_back.add(new fabric.Text(words[0], { 
             fontFamily: font, 
             left: 100, 
             top: 100 
           }));
+
+        //canvas_back.renderAll();
     }
 }
 
