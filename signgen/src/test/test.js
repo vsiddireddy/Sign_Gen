c63c55d63072fc55d8693e8dd541ba173a0c30b7
Vishal Siddireddy <vsiddireddy@gmail.com>
2023-03-07 18:16:07 -0700

    fixed logo spacing and updated fabric.js version

--- signgen/src/test/test.js
+++ signgen/src/test/test.js
@@ -1,67 +1,47 @@
 var sign = new fabric.Canvas('canvas_0');
-
 sign.backgroundColor = "green";
 
-var w = 633;
-var h = 291;
+var w = 913;
+var h = 450;
 var fontSize = w / 12;
 
-var vtop = 0;
-var mainText = new fabric.Textbox("Testing", {
-  top: vtop,
+//To get value for mainText.height
+var testText = new fabric.Textbox("Testing", {
   fontSize: fontSize,
   width: Number(w),
   textAlign: "center",
-  fill: "red",
 });
 
-vtop += mainText.calcTextHeight();
+var vtop = testText.height*2;
+var mainText = new fabric.Textbox("Testing", {
+    top: vtop,
+    fontSize: fontSize,
+    width: Number(w),
+    textAlign: "center",
+    fill: "red",
+});
+
+vtop += mainText.height;
 
 var subText = new fabric.Textbox("Subtext", {
-  top: vtop,
-  fontSize: fontSize/2,
-  width: Number(w),
-  textAlign: "center",
-  fill: "red",
+    top: vtop,
+    fontSize: fontSize/2,
+    width: Number(w),
+    textAlign: "center",
+    fill: "red",
 });
 
-var svgURL = '/signgen/assets/corporate/logos/SVG/JRO_D_Basic_' + 1 + '.svg';
-var svgVar;
-var group = new fabric.Group([], {});
-fabric.loadSVGFromURL(svgURL, function(objects) {
-  svgVar = objects[0].set({
-      top: 0,
-      fill: "blue"
-  });
-  svgVar.scaleToWidth(w/3);
-  svgVar.scaleToHeight(h/3);
-  svgVar.setCoords();
-  sign.add(svgVar);
-  svgVar.centerH();
+var pngURL = '/signgen/assets/corporate/logos/PNG/JRO_D_Basic_1.png';
+
+fabric.Image.fromURL(pngURL, function(img) {
+
+    img.scaleToHeight(mainText.height * 2);
+    img.scaleToWidth(mainText.height  * 2);
+    sign.viewportCenterObjectH(img);
+    img.set({
+      top:  0,
+    });
+    var group = new fabric.Group([mainText, subText, img], {});
+    sign.viewportCenterObjectV(group);
+    sign.add(group);
 });
-
-sign.add(mainText);
-sign.add(subText);
-
-var objs = sign.getObjects();
-
-var group = new fabric.Group(objs);
-
-sign.centerObject(group);
-
-sign.add(group);
-
-sign.renderAll();
-//sign.setActiveGroup(group).renderAll();
-
-/*
-//group = new fabric.Group([ mainText, subText ]);
-var wordArr = [svgVar, mainText, subText];
-wordArr.filter(item => typeof item !== undefined).forEach(item => {group.addWithUpdate(item)});
-console.log(group);
-
-sign.centerObject(group);
-group.setCoords();
-
-sign.setActiveGroup(group).renderAll();
-*/
