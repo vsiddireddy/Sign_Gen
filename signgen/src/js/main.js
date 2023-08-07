42465ac8cfeec4d552adc0b3819aad8b6931f4f4
Vishal Siddireddy <vsiddireddy@gmail.com>
2023-08-06 17:02:34 -0700

    sign grids

--- signgen/src/js/main.js
+++ signgen/src/js/main.js
@@ -286,22 +286,22 @@ document.getElementById("splashGen2").addEventListener("click", function(){
 });
 
 document.getElementById("zmin").addEventListener("click", function(){
-    var elements = document.getElementsByClassName('canvas-container');
-    var csize = getComputedStyle(elements[0]).zoom;
-    elements[0].style.zoom = csize * 1.2;
-    for (var x = 1; x < elements.length; x++) {
-        console.log(elements[x]);
-        elements[x].style.zoom = csize * 1.2;
+    var element = document.getElementById('mainCanvas');
+    var csize = getComputedStyle(element).zoom;
+    element.style.zoom = csize * 1.2;
+    for (var x = 1; x < 1; x++) {
+       //console.log(elements[x]);
+        //element.style.zoom = csize * 1.2;
     }
 });
 
 document.getElementById("zmout").addEventListener("click", function(){
-    var elements = document.getElementsByClassName('canvas-container');
-    var csize = getComputedStyle(elements[0]).zoom;
-    elements[0].style.zoom = csize / 1.2;
-    for (var x = 1; x < elements.length; x++) {
-        console.log(elements[x]);
-        elements[x].style.zoom = csize / 1.2;
+    var element = document.getElementById('mainCanvas');
+    var csize = getComputedStyle(element).zoom;
+    element.style.zoom = csize / 1.2;
+    for (var x = 1; x < 1; x++) {
+        //console.log(elements[x]);
+        //elements[x].style.zoom = csize / 1.2;
     }
 });
 
