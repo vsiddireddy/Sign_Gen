874afa0629cd3435fb6c4c52718cb664c260ebc3
sreegajula <realpopnerd@gmail.com>
2023-08-07 20:24:06 -0700

    ui update

--- signgen/src/js/ui.js
+++ signgen/src/js/ui.js
@@ -1,5 +1,38 @@
 var generate_menu = document.getElementById('generate_menu'),
-    modify_menu = document.getElementById('modify_menu');
+    modify_menu = document.getElementById('modify_menu'),
+    bottom_menu = document.getElementById('bottom_menu');
+
+var home_button = document.getElementById('lm-home'),
+    generate_button = document.getElementById('lm-refresh'),
+    modify_button = document.getElementById('lm-modify'),
+    download_button = document.getElementById('lm-dl');
+
+function resetColorsExcept(button) {
+  var buttons = [home_button, generate_button, modify_button, download_button];
+  
+  for (var i = 0; i < buttons.length; i++) {
+      if(buttons[i] !== button) {
+          buttons[i].style.color = 'rgb(86, 89, 93)';
+      } else {
+          buttons[i].style.color = 'white'; 
+      }
+  }
+}
+
+function switchToHome() {
+  resetColorsExcept(home_button);
+}
+
+function switchToGenerate() {
+  console.log("generatemenu");
+  if(!generate_menu) return;
+  if (getComputedStyle(generate_menu).display == 'none') {
+    generate_menu.style.display = 'block';
+    modify_menu.style.display = 'none';
+    bottom_menu.style.display = 'block';
+  }
+  resetColorsExcept(generate_button);
+}
 
 function switchToModify() {
   console.log("modifymenu");
@@ -7,18 +40,16 @@ function switchToModify() {
   if (getComputedStyle(modify_menu).display == 'none') {
     modify_menu.style.display = 'block';
     generate_menu.style.display = 'none';
+    bottom_menu.style.display = 'none';
   }
+  resetColorsExcept(modify_button);
 }
 
-function switchToGenerate() {
-    console.log("generatemenu");
-    if(!generate_menu) return;
-    if (getComputedStyle(generate_menu).display == 'none') {
-      generate_menu.style.display = 'block';
-      modify_menu.style.display = 'none';
-    }
-  }
+function switchToDownload() {
+  resetColorsExcept(download_button);
+}
 
-document.getElementById('lm-modify').addEventListener('click', switchToModify);
-document.getElementById('lm-refresh').addEventListener('click', switchToGenerate);
-
+home_button.addEventListener('click', switchToHome);
+generate_button.addEventListener('click', switchToGenerate);
+modify_button.addEventListener('click', switchToModify);
+download_button.addEventListener('click', switchToDownload);
